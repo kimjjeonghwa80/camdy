f72f5368fc30f6caa8caedb0518f944414549cce
Azeem Malik <azeemamanatali@gmail.com>
2016-09-06 02:58:39 +0500

    set detail view name, description color sizes

--- web/js/app.controller.js
+++ web/js/app.controller.js
@@ -1,101 +1,117 @@
-//var API_URL = "http://localhost:8000/";
-var API_URL = "https://api.dropp.photo/"; 
+// var API_URL = "http://localhost:8000/"
+var API_URL = 'https://api.dropp.photo/'
 
 angular.module('app.controller', [])
 
-    .controller('blogCtrl', function($scope, $http) {
-        $http({
-            method: "GET",
-            url: "../laravel/public/api/v1/blogs"
-        }).then(function mySuccess(response) {
-            $scope.blogs = response.data;
-        }, function myError(response) {
-            $scope.blogs = response.status + response.statusText;
-        });
-        $scope.msg = "blog";
+  .controller('blogCtrl', function ($scope, $http) {
+    $http({
+      method: 'GET',
+      url: '../laravel/public/api/v1/blogs'
+    }).then(function mySuccess (response) {
+      $scope.blogs = response.data
+    }, function myError (response) {
+      $scope.blogs = response.status + response.statusText
     })
-    //we use stateParams instead of routeParams because we using ui-route
-    .controller('blogDetailCtrl', function($scope, $http, $stateParams) {
-        $http({
-            method: "GET",
-            //url: "http://camdy.app/laravel/public/api/v1/blogs/" + $stateParams.id
-            url: "../laravel/public/api/v1/blogs/" + $stateParams.id
-        }).then(function mySuccess(response) {
-            $scope.blogs = response.data.data;
-            $scope.titles= response.data.data.title;
-                                     
-
-        }, function myError(response) {
-            $scope.blogs = response.status + response.statusText;
-        });
-        $scope.msg = "blog Detail";
+    $scope.msg = 'blog'
+  })
+  // we use stateParams instead of routeParams because we using ui-route
+  .controller('blogDetailCtrl', function ($scope, $http, $stateParams) {
+    $http({
+      method: 'GET',
+      // url: "http://camdy.app/laravel/public/api/v1/blogs/" + $stateParams.id
+      url: '../laravel/public/api/v1/blogs/' + $stateParams.id
+    }).then(function mySuccess (response) {
+      $scope.blogs = response.data.data
+      $scope.titles = response.data.data.title
+    }, function myError (response) {
+      $scope.blogs = response.status + response.statusText
     })
-    .controller('ContactController', function ($scope, $http) {
+    $scope.msg = 'blog Detail'
+  })
+  .controller('ContactController', function ($scope, $http) {
     $scope.result = 'hidden'
-    $scope.resultMessage;
-    $scope.formData; //formData is an object holding the name, email, subject, and message
-    $scope.submitButtonDisabled = false;
-    $scope.submitted = false; //used so that form errors are shown only after the form has been submitted
-    $scope.submit = function(contactform) {
-        $scope.submitted = true;
-        $scope.submitButtonDisabled = true;
-        if (contactform.$valid) {
-            $http({
-                method  : 'POST',
-                url     : 'contact-form.php',
-                data    : $.param($scope.formData),  //param method from jQuery
-                headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  //set the headers so angular passing info as form data (not request payload)
-            }).success(function(data){
-                console.log(data);
-                if (data.success) { //success comes from the return json object
-                    $scope.submitButtonDisabled = true;
-                    $scope.resultMessage = data.message;
-                    $scope.result='bg-success';
-                } else {
-                    $scope.submitButtonDisabled = false;
-                    $scope.resultMessage = data.message;
-                    $scope.result='bg-danger';
-                }
-            });
-        } else {
-            $scope.submitButtonDisabled = false;
-            $scope.resultMessage = 'Error :( Technical error.';
-            $scope.result='bg-danger';
-        }
-    }
-})
-    .controller('printshopCtrl', function($scope, $http) {
+    $scope.resultMessage
+    $scope.formData; // formData is an object holding the name, email, subject, and message
+    $scope.submitButtonDisabled = false
+    $scope.submitted = false // used so that form errors are shown only after the form has been submitted
+    $scope.submit = function (contactform) {
+      $scope.submitted = true
+      $scope.submitButtonDisabled = true
+      if (contactform.$valid) {
         $http({
-            method: "GET",
-            url: API_URL + "merchandise/?order=sort"
-        }).then(function mySuccess(response) {
-            $scope.products = response.data;
-        }, function myError(response) {
-            $scope.products = response.status + response.statusText;
-        });
-        $scope.msg = "PrintShop";
-    })    
+          method: 'POST',
+          url: 'contact-form.php',
+          data: $.param($scope.formData), // param method from jQuery
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' } // set the headers so angular passing info as form data (not request payload)
+        }).success(function (data) {
+          console.log(data)
+          if (data.success) { // success comes from the return json object
+            $scope.submitButtonDisabled = true
+            $scope.resultMessage = data.message
+            $scope.result = 'bg-success'
+          } else {
+            $scope.submitButtonDisabled = false
+            $scope.resultMessage = data.message
+            $scope.result = 'bg-danger'
+          }
+        })
+      } else {
+        $scope.submitButtonDisabled = false
+        $scope.resultMessage = 'Error :( Technical error.'
+        $scope.result = 'bg-danger'
+      }
+    }
+  })
+  .controller('printshopCtrl', function ($scope, $http) {
+    $http({
+      method: 'GET',
+      url: API_URL + 'merchandise/?order=sort'
+    }).then(function mySuccess (response) {
+      $scope.products = response.data
+    }, function myError (response) {
+      $scope.products = response.status + response.statusText
+    })
+    $scope.msg = 'PrintShop'
+  })
 
-     .controller('printshopCategoryCtrl', function($scope,  $stateParams, $http) {
-         $http({
-            method: "GET",
-            url: API_URL + "merchandise/" + $stateParams.category
-        }).then(function mySuccess(response) {
-            $scope.products = response.data;
-        }, function myError(response) {
-            $scope.products = response.status + response.statusText;
-        });
-        $scope.msg = "PrintShop Detail";
-    })  
+  .controller('printshopCategoryCtrl', function ($scope, $stateParams, $http) {
+    $http({
+      method: 'GET',
+      url: API_URL + 'merchandise/' + $stateParams.category
+    }).then(function mySuccess (response) {
+      $scope.products = response.data
+    }, function myError (response) {
+      $scope.products = response.status + response.statusText
+    })
+    $scope.category = $stateParams.category
+    $scope.msg = 'PrintShop Detail'
+  })
 
-         .controller('printshopDetailCtrl', function($scope,  $stateParams, $http, $state) {
-         $http({
-            method: "GET",
-            url: API_URL + "merchandise/" +  $state.current + "/" + $stateParams.product 
-        }).then(function mySuccess(response) {
-            $scope.products = response.data;
-        }, function myError(response) {
-            $scope.products = response.status + response.statusText;
-        });
-        $scope.msg = "PrintShop View";
-    })          
+  .controller('printshopDetailCtrl', function ($scope, $sce, $stateParams, $http, $state) {
+    $http({
+      method: 'GET',
+      url: API_URL + 'merchandise/' + $stateParams.category + '/' + $stateParams.product
+    }).then(function mySuccess (response) {
+      $scope.product = response.data[0];
+      $scope.currentSize = $scope.product.sizes.S;      
+    }, function myError (response) {
+      $scope.product = response.status + response.statusText;
+    })
+
+    $scope.setCurrentSize = function (item) {
+      $scope.currentSize = item
+    }
+
+    $scope.getColor = function (color) {
+      console.log(color, 'color')
+      return {
+        'background-color': color + '!important'
+      }
+    }
+
+    $scope.deliberatelyTrustDangerousSnippet = function (htmlString) {
+      return $sce.trustAsHtml(htmlString)
+    }
+
+    $scope.msg = 'PrintShop View'
+  })
