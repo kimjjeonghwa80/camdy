cb5a00ac62c053e1ed6c03a830d5093bee49f620
Azeem Malik <azeemamanatali@gmail.com>
2016-09-22 01:17:56 +0500

    add text start

--- myapp/src/app/js/app.controller.js
+++ myapp/src/app/js/app.controller.js
@@ -97,20 +97,3 @@ angular.module('app.controller', [])
     $scope.category = $stateParams.category
     $scope.msg = 'PrintShop Detail'
   })
-
-  .controller('customizeCtrl', function ($scope, $http, localStorageService) {
-    $scope.msg = 'Customize the product'
-    $scope.selectedItemObject = localStorageService.get('selectedItem');
-    $scope.selectedItem = $scope.selectedItemObject.template_image;
-    $scope.changeSelectedItem = function (img) {
-      $scope.selectedItem = img
-    }
-    $scope.selectedItemDimensions = localStorageService.get('selectedItemDimensions')
-    $scope.maxWidth = 300
-    $scope.maxHeight = 300
-    $scope.x = $scope.selectedItemDimensions.x
-    $scope.y = $scope.selectedItemDimensions.y
-    // $scope.ratio =  $scope.maxWidth/$scope.selectedItemDimensions.width
-    $scope.canvasWidth =  $scope.selectedItemDimensions.width
-    $scope.canvasHeight =  $scope.selectedItemDimensions.height
-  })
