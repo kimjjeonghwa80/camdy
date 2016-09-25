bb5627dcabedae2bc64b0f2a35f0b6556fda4565
Azeem Malik <azeemamanatali@gmail.com>
2016-09-25 20:19:02 +0500

    removed upto product detail

--- myapp/src/app/js/ProductDetailController.js
+++ myapp/src/app/js/ProductDetailController.js
@@ -50,24 +50,3 @@ $stateParams, $http, $state, $rootScope, localStorageService) {
       $scope.activeImage = img
     }
   })
-//  .controller('customizeCtrl', function ($scope, $http, localStorageService, $rootScope) {
-//      $rootScope.canvas = new fabric.Canvas('designer-canvas', {
-//     hoverCursor: 'pointer',
-//     selection: true
-//   });
-
-//     $scope.msg = 'Customize the product'
-//     $scope.selectedItemObject = localStorageService.get('selectedItem');
-//     $scope.selectedItem = $scope.selectedItemObject.template_image;
-//     $scope.changeSelectedItem = function (img) {
-//       $scope.selectedItem = img
-//     }
-//     $scope.selectedItemDimensions = localStorageService.get('selectedItemDimensions')
-//     $scope.maxWidth = 300
-//     $scope.maxHeight = 300
-//     $scope.x = $scope.selectedItemDimensions.x
-//     $scope.y = $scope.selectedItemDimensions.y
-//     // $scope.ratio =  $scope.maxWidth/$scope.selectedItemDimensions.width
-//     $scope.canvasWidth =  $scope.selectedItemDimensions.width
-//     $scope.canvasHeight =  $scope.selectedItemDimensions.height
-//   })
